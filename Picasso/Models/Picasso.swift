67289f52308b7e2f5f288d44de828363e599f2b7
Pave1iOS <PGribachev94@yandex.ru>
2024-03-02 01:27:08 +0300

    remove depricated prop by model

--- Picasso/Models/Picasso.swift
+++ Picasso/Models/Picasso.swift
@@ -18,15 +18,9 @@ struct Picasso: Decodable {
     let height: Int
     let color: String
     let blur_hash: String
-//    let location: Location
     let urls: Urls
     let links: Links
 }
-
-//struct Location: Decodable {
-//    let city: String
-//    let country: String
-//}
 
 struct Urls: Decodable {
     let raw: String
